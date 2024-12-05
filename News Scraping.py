a8bbd7dc2ef6a23ed8585194befae2752f0f4cab
Samantha Pui <samanthampui@Samanthas-MacBook-Pro-10.local>
2024-12-04 19:57:15 -0500

    Samantha working on news scraping API

--- News Scraping.py
+++ News Scraping.py
@@ -2,4 +2,30 @@ import requests
 import json
 import sqlite3
 
-audio_API = ''
+ACCESS_KEY = 'e833f1cc919ad792e91a2c2a803c3b3c'
+
+def create_news_database():
+
+    params = {
+        'access_key': 'ACCESS_KEY',
+        'categories': 'general,health',
+        'countries': 'us',
+        'keywords': 'virus,corona',
+        'date': '2021-01-24,2022-12-31'
+        }
+
+    url = f'''https://api.mediastack.com/v1/news?
+        access_key=e833f1cc919ad792e91a2c2a803c3b3c&categories=general,health&countries=us&keywords=virus,covid
+        &date=2021-01-01,2022-12-31'''
+    
+    response = requests.get(url)
+    news_dict = json.loads(response.content)
+
+    print(news_dict)
+
+    pass
+
+def main():
+    create_news_database()
+
+main()
