1c5ce2d478f8bbf56d32b90095805d4125c54fad
Samantha Pui <samanthampui@Samanthas-MacBook-Pro-10.local>
2024-12-14 18:08:49 -0500

    New key for News API

--- News Scraping.py
+++ News Scraping.py
@@ -5,7 +5,7 @@ import http.client, urllib.parse
 import re
 import os
 
-ACCESS_KEY = '8b5020c63b59809c8b3d2ee19b0fcc4f'
+ACCESS_KEY = '98ccceb701f523f653f3ea662ec39fdc'
 
 def set_up_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -19,7 +19,7 @@ def create_news_dict():
         'access_key': ACCESS_KEY,
         'categories': 'general',
         'countries': 'us',
-        'keywords': 'climate change',
+        'keywords': 'cyclone',
         'limit': 100,
         'sort': 'popularity'
         })
@@ -61,41 +61,7 @@ def create_news_database(dict, cur, conn):
     pass
 
 def main():
-    #news_dict = create_news_dict()
-    news_dict = {
-        "pagination": {
-        "limit": 100,
-        "offset": 0,
-        "count": 100,
-        "total": 1332
-    },
-    "data": [
-        {
-        "author": "Jerel Ezell",
-        "title": "How Youth Climate Anxiety Became a Convenient Foil for the Right",
-        "description": "Suppression of climate change discussions is likely to only elevate more fear around it, writes Jerel Ezell.",
-        "url": "https://time.com/7093781/youth-climate-anxiety-weaponization-essay/",
-        "source": "TIME.com",
-        "image": None,
-        "category": "general",
-        "language": "en",
-        "country": "us",
-        "published_at": "2024-10-16T15:45:40+00:00"
-        },
-        {
-      "author": "Austyn Gaffney and Somini Sengupta",
-      "title": "Floods Wreak Havoc Across Four Continents",
-      "description": "Flooding events around the world share a common factor of an atmosphere made warmer by climate change. What can be done to help citizens prepare?",
-      "url": "https://www.nytimes.com/2024/09/18/climate/global-flooding-climate-change.html",
-      "source": "The New York Times",
-      "image": "https://static01.nyt.com/images/2024/09/18/multimedia/18cli-global-flooding-01-cbtp/18cli-global-flooding-01-cbtp-mediumSquareAt3X.jpg",
-      "category": "general",
-      "language": "en",
-      "country": "us",
-      "published_at": "2024-09-18T18:33:18+00:00"
-    }
-    ]
-    }
+    news_dict = create_news_dict()
     curr, con = set_up_database('main.db')
     create_news_database(news_dict, curr, con)
 
