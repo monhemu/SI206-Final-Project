ddab16ea72b026410617982ce036cf88af8f7875
Samantha Pui <samanthampui@Samanthas-MacBook-Pro-10.local>
2024-12-15 23:25:32 -0500

    working on visualizations for news

--- Visualizations.py
+++ Visualizations.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
 import sqlite3
 
 conn = sqlite3.connect('main.db')
@@ -14,7 +16,6 @@ cur.execute("""
 
 # Fetch results
 results = cur.fetchall()
-conn.close()
 
 # Extract data for visualization
 artist_names = [row[0] for row in results]
@@ -30,4 +31,40 @@ plt.gca().invert_yaxis()
 plt.tight_layout()
 
 #plt.savefig("songs_per_artist.png")
+#plt.show()
+
+#visualization for artist news / charted songs
+cur.execute('''SELECT artists.name, artists.id, COUNT(songs.id) as song_count
+                FROM artists
+                JOIN songs ON artists.id = songs.artist_id
+                GROUP BY artists.name
+                ORDER BY song_count DESC
+                LIMIT 5''')
+
+top_5_artists = cur.fetchall()
+artist_dict = {}
+for artist in top_5_artists:
+    artist_dict[artist[0]] = {}
+    cur.execute('''SELECT date FROM songs
+                    WHERE artist_id = ?''',
+                    (artist[1],))
+    song_dates = cur.fetchall()
+    print(song_dates)
+    for date in song_dates:
+            
+        if date[0] not in artist_dict[artist[0]]:
+            artist_dict[artist[0]][date[0]] = 1
+        else:
+            artist_dict[artist[0]][date[0]] += 1
+
+plt.figure(figsize=(15, 10))
+
+for artist in artist_dict:
+    dates = artist_dict[artist].keys()
+    values = artist_dict[artist].values()
+    plt.scatter(dates, values)
+#create line plot
 plt.show()
+conn.close()
+
+pass
